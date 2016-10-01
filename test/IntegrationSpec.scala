f85cfb15dc258648e479f0655f145a5809b45d01
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-10-01 12:52:43 -0500

    Format

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,20 +1,18 @@
 import org.scalatestplus.play._
-import play.api.test._
-import play.api.test.Helpers._
 
 /**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
+  * add your integration spec here.
+  * An integration test will fire up a whole play application in a real (or headless) browser
+  */
 class IntegrationSpec extends PlaySpec with OneServerPerTest with OneBrowserPerTest with HtmlUnitFactory {
 
-  "Application" should {
+	"Application" should {
 
-    "work from within a browser" in {
+		"work from within a browser" in {
 
-      go to ("http://localhost:" + port + "/user")
+			go to ("http://localhost:" + port + "/user")
 
-      pageSource must include ("User controller")
-    }
-  }
+			pageSource must include("User controller")
+		}
+	}
 }
