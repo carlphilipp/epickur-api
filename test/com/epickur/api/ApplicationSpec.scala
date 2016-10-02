9e3773d29e13c236bbb37c922a70b7a44f92d048
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-10-02 14:45:22 -0500

    Move user controller test

--- test/com/epickur/api/ApplicationSpec.scala
+++ test/com/epickur/api/ApplicationSpec.scala
@@ -1,6 +1,9 @@
 package com.epickur.api
 
+import com.epickur.api.entity.User
+import com.epickur.api.utils.TestUtils
 import org.scalatestplus.play._
+import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
@@ -14,6 +17,44 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
 	"Routes" should {
 		"Send 404 on a bad request" in {
 			route(app, FakeRequest(GET, "/boomz")).map(status) mustBe Some(NOT_FOUND)
+		}
+	}
+
+	"User" should {
+		"Get user" in {
+			val home = route(app, FakeRequest(GET, s"/users/${TestUtils.id}")).get
+			status(home) mustBe OK
+			contentType(home) mustBe Some(JSON)
+			val user = contentAsJson(home).as[User]
+			TestUtils.verifyUser(user)
+		}
+	}
+
+	"User" should {
+		"Create user" in {
+			val fakeRequest = FakeRequest(POST, "/users")
+				.withJsonBody(Json.parse(TestUtils.userAsString))
+				.withHeaders(CONTENT_TYPE -> JSON)
+
+			val home = route(app, fakeRequest).get
+			status(home) mustBe OK
+			contentType(home) mustBe Some(JSON)
+			val user = contentAsJson(home).as[User]
+			TestUtils.verifyUser(user)
+		}
+	}
+
+	"User" should {
+		"Update user" in {
+			val fakeRequest = FakeRequest(PUT, s"/users/${TestUtils.id}")
+				.withJsonBody(Json.parse(TestUtils.userAsString))
+				.withHeaders(CONTENT_TYPE -> JSON)
+
+			val home = route(app, fakeRequest).get
+			status(home) mustBe OK
+			contentType(home) mustBe Some(JSON)
+			val user = contentAsJson(home).as[User]
+			TestUtils.verifyUser(user)
 		}
 	}
 }
