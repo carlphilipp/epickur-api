e7ac4a1f5f6b2362f92f2bfa2d73f1cc4d53555b
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-10-02 11:46:26 -0500

    Creat user controller test

--- test/com/epickur/ApplicationSpec.scala
+++ test/com/epickur/ApplicationSpec.scala
@@ -1,9 +1,6 @@
 package com.epickur
 
-import com.epickur.entity.User
-import com.epickur.utils.TestUtils
 import org.scalatestplus.play._
-import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
@@ -18,58 +15,5 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
 		"Send 404 on a bad request" in {
 			route(app, FakeRequest(GET, "/boomz")).map(status) mustBe Some(NOT_FOUND)
 		}
-	}
-
-	"UserController" should {
-		"Get user" in {
-			val home = route(app, FakeRequest(GET, s"/users/${TestUtils.id}")).get
-			status(home) mustBe OK
-			contentType(home) mustBe Some(JSON)
-			val user = contentAsJson(home).as[User]
-			verifyUser(user)
-		}
-	}
-
-	"UserController" should {
-		"Create user" in {
-			val fakeRequest = FakeRequest(POST, "/users")
-				.withJsonBody(Json.parse(TestUtils.user))
-				.withHeaders(CONTENT_TYPE -> JSON)
-
-			val home = route(app, fakeRequest).get
-			status(home) mustBe OK
-			contentType(home) mustBe Some(JSON)
-			val user = contentAsJson(home).as[User]
-			verifyUser(user)
-		}
-	}
-
-	"UserController" should {
-		"Update user" in {
-			val fakeRequest = FakeRequest(PUT, "/users")
-				.withJsonBody(Json.parse(TestUtils.user))
-				.withHeaders(CONTENT_TYPE -> JSON)
-
-			val home = route(app, fakeRequest).get
-			status(home) mustBe OK
-			contentType(home) mustBe Some(JSON)
-			val user = contentAsJson(home).as[User]
-			verifyUser(user)
-		}
-	}
-
-	def verifyUser(user: User) = {
-		assert(user.id.getOrElse(fail) == TestUtils.id.toLong)
-		assert(user.name == TestUtils.name)
-		assert(user.first == TestUtils.first)
-		assert(user.last == TestUtils.last)
-		assert(user.password == TestUtils.password)
-		assert(user.email == TestUtils.email)
-		assert(user.zipcode == TestUtils.zipcode)
-		assert(user.state == TestUtils.state)
-		assert(user.country == TestUtils.country)
-		assert(user.allow == TestUtils.allow.toInt)
-		assert(user.createdAt != null)
-		assert(user.updatedAt != null)
 	}
 }
---
+++ test/com/epickur/controllers/UserControllerTest.scala
@@ -0,0 +1,64 @@
+package com.epickur.controllers
+
+import com.epickur.entity.User
+import com.epickur.utils.TestUtils
+import org.scalatestplus.play._
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class UserControllerTest extends PlaySpec with OneAppPerTest {
+
+	"UserController" should {
+		"Get user" in {
+			val home = route(app, FakeRequest(GET, s"/users/${TestUtils.id}")).get
+			status(home) mustBe OK
+			contentType(home) mustBe Some(JSON)
+			val user = contentAsJson(home).as[User]
+			verifyUser(user)
+		}
+	}
+
+	"UserController" should {
+		"Create user" in {
+			val fakeRequest = FakeRequest(POST, "/users")
+				.withJsonBody(Json.parse(TestUtils.user))
+				.withHeaders(CONTENT_TYPE -> JSON)
+
+			val home = route(app, fakeRequest).get
+			status(home) mustBe OK
+			contentType(home) mustBe Some(JSON)
+			val user = contentAsJson(home).as[User]
+			verifyUser(user)
+		}
+	}
+
+	"UserController" should {
+		"Update user" in {
+			val fakeRequest = FakeRequest(PUT, "/users")
+				.withJsonBody(Json.parse(TestUtils.user))
+				.withHeaders(CONTENT_TYPE -> JSON)
+
+			val home = route(app, fakeRequest).get
+			status(home) mustBe OK
+			contentType(home) mustBe Some(JSON)
+			val user = contentAsJson(home).as[User]
+			verifyUser(user)
+		}
+	}
+
+	def verifyUser(user: User) = {
+		assert(user.id.getOrElse(fail) == TestUtils.id.toLong)
+		assert(user.name == TestUtils.name)
+		assert(user.first == TestUtils.first)
+		assert(user.last == TestUtils.last)
+		assert(user.password == TestUtils.password)
+		assert(user.email == TestUtils.email)
+		assert(user.zipcode == TestUtils.zipcode)
+		assert(user.state == TestUtils.state)
+		assert(user.country == TestUtils.country)
+		assert(user.allow == TestUtils.allow.toInt)
+		assert(user.createdAt != null)
+		assert(user.updatedAt != null)
+	}
+}
