7a06542d1429b793fed1403a6d826a2f304d2b70
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-10-14 17:53:19 -0500

    Update service test

--- test/com/epickur/api/services/UserServiceTest.scala
+++ test/com/epickur/api/services/UserServiceTest.scala
@@ -1,7 +1,7 @@
 package com.epickur.api.services
 
 import com.epickur.api.dao.UserDAO
-import com.epickur.api.entities.User
+import com.epickur.api.entities.{Role, User}
 import com.epickur.api.utils.TestUtils
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
@@ -25,12 +25,31 @@ class UserServiceTest extends FeatureSpec with GivenWhenThen with Matchers with 
 		scenario("Create a user") {
 			Given("a new user")
 			val user = TestUtils.user
+			val originalPassword = user.password
 
 			When("passing it to the create service")
 			val actual = userService.create(user)
 
 			Then("the service returns a Future[unit]")
 			assert(actual.isInstanceOf[Future[Unit]])
+
+			Then("a random user id is generated")
+			assert(user.id != null)
+
+			Then("the user role is 'user'")
+			assert(user.role.isDefined)
+			assert(user.role.get == Role.user)
+
+			Then("the user creation date is not null")
+			assert(user.createdAt.isDefined)
+			assert(user.createdAt.get != null)
+
+			Then("the user update date is not null")
+			assert(user.updatedAt.isDefined)
+			assert(user.updatedAt.get != null)
+
+			Then("the user password is encoded")
+			assert(user.password != originalPassword)
 
 			Then("the dao layer has been called for user creation")
 			verify(userDAO).create(TestUtils.user)
@@ -59,6 +78,28 @@ class UserServiceTest extends FeatureSpec with GivenWhenThen with Matchers with 
 
 			Then("the service returns a Future[Unit]")
 			assert(actual.isInstanceOf[Future[Unit]])
+
+			Then("the user password is null")
+			assert(user.password == null)
+
+			Then("the user roll is empty")
+			assert(user.role.isEmpty)
+
+			Then("the user allow is empty")
+			assert(user.allow.isEmpty)
+
+			Then("the user key is empty")
+			assert(user.key.isEmpty)
+
+			Then("the user newPassword is empty")
+			assert(user.newPassword.isEmpty)
+
+			Then("the user creation date must not be modified")
+			assert(user.createdAt.isEmpty)
+
+			Then("the user update date is not null")
+			assert(user.updatedAt.isDefined)
+			assert(user.updatedAt.get != null)
 
 			Then("the dao layer has been called for user update")
 			verify(userDAO).update(TestUtils.user)
