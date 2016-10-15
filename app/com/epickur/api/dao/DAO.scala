9343b31dd1bbb6d5ded456183f067b0e6eaa4be4
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-10-14 20:48:14 -0500

    Refactor dao

--- app/com/epickur/api/dao/DAO.scala
+++ app/com/epickur/api/dao/DAO.scala
@@ -1,13 +1,17 @@
 package com.epickur.api.dao
 
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsObject, Json}
+import play.modules.reactivemongo.json.JsObjectDocumentWriter
 import play.modules.reactivemongo.{MongoController, ReactiveMongoComponents}
+import reactivemongo.api.ReadPreference
 import reactivemongo.play.json.collection.JSONCollection
 
 import scala.concurrent.Future
 
 trait DAO[T] extends MongoController with ReactiveMongoComponents {
+
+	val collection: Future[JSONCollection]
 
 	def loadCollection(collection: String): Future[JSONCollection] = {
 		database.map(_.collection[JSONCollection](collection))
@@ -15,7 +19,9 @@ trait DAO[T] extends MongoController with ReactiveMongoComponents {
 
 	def create(entity: T): Future[Unit]
 
-	def read(id: String): Future[List[JsObject]]
+	def read(id: String): Future[List[JsObject]] = {
+		collection.flatMap(_.find(Json.obj("_id" -> id)).cursor[JsObject](ReadPreference.primary).collect[List](1))
+	}
 
 	def update(entity: T): Future[Unit]
 }
--- app/com/epickur/api/dao/UserDAO.scala
+++ app/com/epickur/api/dao/UserDAO.scala
@@ -4,10 +4,9 @@ import javax.inject.{Inject, Singleton}
 
 import com.epickur.api.entities.User
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.{JsObject, Json, OWrites}
+import play.api.libs.json.{Json, OWrites}
+import play.modules.reactivemongo.json.JsObjectDocumentWriter
 import play.modules.reactivemongo.ReactiveMongoApi
-import play.modules.reactivemongo.json._
-import reactivemongo.api.ReadPreference
 
 import scala.concurrent.Future
 
@@ -16,14 +15,10 @@ class UserDAO @Inject()(val reactiveMongoApi: ReactiveMongoApi) extends DAO[User
 
 	implicit val userToJson: OWrites[User] = User.userToJsonDB
 
-	def collection = loadCollection("users")
+	override val collection = loadCollection("users")
 
 	override def create(user: User): Future[Unit] = {
 		collection.flatMap(_.insert(user)).map(_ => {})
-	}
-
-	override def read(id: String): Future[List[JsObject]] = {
-		collection.flatMap(_.find(Json.obj("_id" -> id)).cursor[JsObject](ReadPreference.primary).collect[List](1))
 	}
 
 	override def update(user: User): Future[Unit] = {
