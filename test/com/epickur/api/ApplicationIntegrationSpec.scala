563ffb1466231c36f9ede0aa0077c07832a0e017
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-10-16 16:42:55 -0500

    Add user update integration test

--- test/com/epickur/api/ApplicationIntegrationSpec.scala
+++ test/com/epickur/api/ApplicationIntegrationSpec.scala
@@ -4,15 +4,17 @@ import com.epickur.api.entities.User
 import com.epickur.api.utils.TestUtils
 import com.epickur.api.utils.TestUtils.verifyUser
 import play.api.Application
-import play.api.libs.json.{Json, Reads}
+import play.api.libs.json.{Json, Reads, Writes}
 import play.api.libs.ws.WS
 import play.api.test.Helpers._
 import play.api.test._
+import reactivemongo.bson.BSONObjectID
 
 
 class ApplicationIntegrationSpec extends PlaySpecification {
 
 	implicit val jsonToUser: Reads[User] = User.jsonToUserWeb
+	implicit val userToJson: Writes[User] = User.userToJsonWeb
 
 	val url = "http://localhost:"
 
@@ -22,21 +24,42 @@ class ApplicationIntegrationSpec extends PlaySpecification {
 			response.status must equalTo(NOT_FOUND)
 		}
 
+		"return 404 when accessing a non existing user" in new WithServer {
+			val response = await(WS.url(url + port + "/users/" + BSONObjectID.generate.stringify).get)
+			response.status must equalTo(NOT_FOUND)
+		}
+
 		"create a user" in new WithServer {
 			createAndValidateUser(port)
 		}
 
-		"read a user" in new WithServer {
+		"get a user" in new WithServer {
 			// Create user
 			val user = createAndValidateUser(port)
 
-			// Update user
+			// Get that user
 			val response = await(WS.url(url + port + "/users/" + user.id.get).get)
 
 			response.status must equalTo(OK)
 			response.json must not be null
 			val userResponse = response.json.as[User]
 			verifyUser(userResponse)
+		}
+
+		"update a user" in new WithServer {
+			// Create user
+			val user = createAndValidateUser(port)
+			val userUpdated = TestUtils.user
+			userUpdated.id = user.id
+			userUpdated.email = "newemail@email.com"
+
+			// Update user
+			val response = await(WS.url(url + port + "/users/" + user.id.get).withHeaders(CONTENT_TYPE -> JSON).put(Json.toJson(userUpdated)))
+
+			response.status must equalTo(OK)
+			response.json must not be null
+			val userResponse = response.json.as[User]
+			assert(userResponse.email == "newemail@email.com")
 		}
 	}
 
